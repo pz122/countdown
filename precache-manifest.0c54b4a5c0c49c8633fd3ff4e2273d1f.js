5cdf8ee61e2051bf7cdf69640fffe9f6167fb049
pz122 <[prishazenia@live.com]>
2019-01-26 14:58:28 +0800

    Updates

---
+++ precache-manifest.0c54b4a5c0c49c8633fd3ff4e2273d1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fe148f8c6ea39c28a69d7d719b25df5",
+    "url": "/static/media/Avenir-Medium-09.0fe148f8.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a910e3b1297a0d403b73",
+    "url": "/static/js/main.a910e3b1.chunk.js"
+  },
+  {
+    "revision": "701e80665e0aa5267d43",
+    "url": "/static/js/1.701e8066.chunk.js"
+  },
+  {
+    "revision": "a910e3b1297a0d403b73",
+    "url": "/static/css/main.bc66cc8e.chunk.css"
+  },
+  {
+    "revision": "3dc7555a777c3779590c91fa738bf55b",
+    "url": "/index.html"
+  }
+];
